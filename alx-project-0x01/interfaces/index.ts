1a24754a84151eb7046035efb31e4430a4da6f41
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-07-20 22:49:45 +0300

    feat: implement add posts modal

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -36,3 +36,14 @@ export interface UserProps {
   company: Company;
 }
 
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
+}
