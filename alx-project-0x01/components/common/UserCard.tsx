2f6e1c3335f08ab377fbb7cc115f91692ca6621d
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-07-20 22:13:01 +0300

    feat: implement UserCard component and display users from API

---
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { UserProps } from "@/interfaces";
+
+
+export default function UserCard({ 
+  name, 
+  email, 
+  phone, 
+  website, 
+  company, 
+  address 
+}): UserProps {
+  return (
+    <div className="p-4 rounded-2xl shadow-md border bg-white">
+      <h2 className="text-xl font-bold mb-2">{name}</h2>
+      <p className="text-sm text-gray-600">📧 {email}</p>
+      <p className="text-sm text-gray-600">📞 {phone}</p>
+      <p className="text-sm text-gray-600">🌐 {website}</p>
+      <p className="text-sm mt-2 font-medium">🏢 {company.name}</p>
+      <p className="text-xs text-gray-500 italic">{company.catchPhrase}</p>
+      <p className="text-sm mt-2">📍 {address.city}, {address.street}</p>
+    </div>
+  );
+}
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,14 +1,32 @@
-import Header from "@/components/layout/Header";
+// import Header from "@/components/layout/Header";
+import UserCard from "@/components/common/UserCard";
+import { UserProps } from "@/interfaces";
 
+type UsersPageProps = {
+  posts: UserProps[];
+};
 
-const Users: React.FC = () => {
-    return (
-        <div>
-            <Header />
-            users
-        </div>
-    )
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
 
 
-export default Users;
+export default function Users({ posts }: UsersPageProps) {
+  return (
+    <div className="p-6 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      {posts.map((user) => (
+        <UserCard 
+            key={user.id} 
+            {...user} 
+        />
+      ))}
+    </div>
+  );
+}
